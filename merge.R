ad5002667b437a698fd9aec25fc2db8f26f30c14
Camillo Colleluori <camillo.colleluori@student.kuleuven.be>
2024-02-18 16:11:01 +0100

    merge edit

--- merge.R
+++ merge.R
@@ -10,5 +10,6 @@ pop = pop[,-c(1,5)]
 #merge dataframes
 final_df_with_pop = inner_join(full_df, pop, by = c("postal_code" = "Post.code"))
 names(final_df_with_pop)[names(final_df_with_pop) == 'CD_REFNIS'] <- 'municipality'
+names(final_df_with_pop)[names(final_df_with_pop) == 'TOTAL'] <- 'population'
 
 write.csv(final_df_with_pop, file = "dataset_with_populations.csv")
